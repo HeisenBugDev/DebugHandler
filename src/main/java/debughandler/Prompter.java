1805bb509f40f7cfb64195de81a75e83d6b8d0bc
hunterboerner <hunterboerner@gmail.com>
2013-12-07 21:09:28 -0600

    text prompter

--- src/main/java/debughandler/Prompter.java
+++ src/main/java/debughandler/Prompter.java
@@ -1,4 +1,24 @@
 package debughandler;
 
-public class Prompter {
+import javax.swing.*;
+import java.awt.*;
+
+public class Prompter extends JFrame{
+
+    public Prompter(String hastebinCode) {
+        setTitle("QuantumCraft Debug");
+        // Use the dafault metal styled titlebar
+        getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+        // Set the style of the frame
+
+        add(new JTextArea("These are the logs we have collected, please give this link to the person " +
+                "helping you solve the problem: http://hastebin.com/" +
+                hastebinCode, 5, 25));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(new FlowLayout());
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
