5b303ef2527aeccd576d0ce32863e5550805e725
hunterboerner <hunterboerner@gmail.com>
2013-12-07 21:10:02 -0600

    register and upload

--- src/main/java/debughandler/DebugRegistry.java
+++ src/main/java/debughandler/DebugRegistry.java
@@ -1,4 +1,19 @@
 package debughandler;
 
+import java.util.ArrayList;
+
 public class DebugRegistry {
+    private static ArrayList<LogHandler> logHandlers = new ArrayList<LogHandler>();
+
+    public static void addLogHandler(LogHandler handler){
+       logHandlers.add(handler);
+    }
+
+    public static void uploadLogs(){
+        try {
+            FileUploader.upload(logHandlers);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
