03e579350f13ea685e896d266d71b976e2ca4ff9
hunterboerner <hunterboerner@gmail.com>
2013-12-07 19:46:11 -0600

    variables

--- src/main/java/debughandler/LogHandler.java
+++ src/main/java/debughandler/LogHandler.java
@@ -1,4 +1,13 @@
 package debughandler;
 
 public class LogHandler {
+    public FileUploader uploader;
+    public Prompter prompter;
+    public FileWriter writer;
+
+    public LogHandler(){
+        uploader = new FileUploader();
+        prompter = new Prompter();
+        writer = new FileWriter();
+    }
 }
